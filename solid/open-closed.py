723ee137f54575e11aad9cfdb62b3e58fa9c59e0
umeshror <sarukumesh@gmail.com>
2019-05-18 10:06:19 +0530

    Open closed principle added

--- solid/open-closed.py
+++ solid/open-closed.py
@@ -1,11 +1,126 @@
-from aenum import Enum
+"""
+The Open Close Principle states that the design and writing of the code should be done in
+ a way that new functionality should be added with minimum changes in the existing code.
+  The design should be done in a way to allow the adding of new functionality as new classes,
+  keeping as much as possible existing code unchanged.
+"""
+from enum import Enum
 
-class Numbers(Enum):
-    ONE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
 
-print(Numbers.ONE)
-print(Numbers.ONE.value)
-print(Numbers.THREE.value)
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+
+class Size(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+
+
+class Product:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.color = color
+        self.size = size
+
+
+# bad practice
+class ProductFilter:
+    def filter_by_color(self, products, color):
+        for p in products:
+            if p.color == color: yield p
+
+    def filter_by_size(self, products, size):
+        for p in products:
+            if p.size == size: yield p
+
+    def filter_by_size_and_color(self, products, size, color):
+        for p in products:
+            if p.color == color and p.size == size:
+                yield p
+
+                # state space explosion
+                # 3 criteria
+
+
+# OCP = open for extension, closed for modification
+class Specification:
+    def is_satisfied(self, item):
+        pass
+
+    # and operator makes life easier
+    def __and__(self, other):
+        return AndSpecification(self, other)
+
+
+class Filter:
+    def filter(self, items, spec):
+        pass
+
+
+class ColorSpecification(Specification):
+    def __init__(self, color):
+        self.color = color
+
+    def is_satisfied(self, item):
+        return item.color == self.color
+
+
+class SizeSpecification(Specification):
+    def __init__(self, size):
+        self.size = size
+
+    def is_satisfied(self, item):
+        return item.size == self.size
+
+
+class AndSpecification(Specification):
+    def __init__(self, spec1, spec2):
+        self.spec2 = spec2
+        self.spec1 = spec1
+
+    def is_satisfied(self, item):
+        return self.spec1.is_satisfied(item) and \
+               self.spec2.is_satisfied(item)
+
+
+class BetterFilter(Filter):
+    def filter(self, items, spec):
+        for item in items:
+            if spec.is_satisfied(item):
+                yield item
+
+
+apple = Product('Apple', Color.GREEN, Size.SMALL)
+tree = Product('Tree', Color.GREEN, Size.LARGE)
+house = Product('House', Color.BLUE, Size.LARGE)
+
+products = [apple, tree, house]
+
+pf = ProductFilter()
+print('Green products (old):')
+for p in pf.filter_by_color(products, Color.GREEN):
+    print(f' - {p.name} is green')
+
+# ^ BEFORE
+
+# v AFTER
+bf = BetterFilter()
+
+print('Green products (new):')
+green = ColorSpecification(Color.GREEN)
+for p in bf.filter(products, green):
+    print(f' - {p.name} is green')
+
+print('Large products:')
+large = SizeSpecification(Size.LARGE)
+for p in bf.filter(products, large):
+    print(f' - {p.name} is large')
+
+print('Large blue items:')
+# large_blue = AndSpecification(large, ColorSpecification(Color.BLUE))
+large_blue = large and ColorSpecification(Color.BLUE)
+for p in bf.filter(products, large_blue):
+    print(f' - {p.name} is large and blue')
