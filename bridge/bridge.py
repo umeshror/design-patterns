48aa7c64424144ce5d4d7cff8b032c29105b9bc2
umeshror <sarukumesh@gmail.com>
2019-06-08 14:29:56 +0530

    bridge added

---
+++ bridge/bridge.py
@@ -0,0 +1,58 @@
+"""
+The Bridge design pattern allows you to separate the abstraction from the implementation.
+It is a structural design pattern.
+
+Decouple an abstraction from its implementation so that the two can vary independently
+
+A mechanism that decouples the interface(hierarchy) from the implementation(hierarchy)
+"""
+
+import unittest
+from abc import ABC
+
+
+class Renderer(ABC):
+    @property
+    def what_to_render_as(self):
+        return None
+
+
+class Shape(ABC):
+    def __init__(self, renderer, name):
+        self.renderer = renderer
+        self.name = name
+
+    def __str__(self):
+        return 'Drawing %s as %s' % (self.name, self.renderer.what_to_render_as)
+
+
+class Triangle(Shape):
+    def __init__(self, renderer):
+        super().__init__(renderer, 'Triangle')
+
+
+class Square(Shape):
+    def __init__(self, renderer):
+        super().__init__(renderer, 'Square')
+
+
+class RasterRenderer(Renderer):
+    @property
+    def what_to_render_as(self):
+        return 'pixels'
+
+
+class VectorRenderer(Renderer):
+    @property
+    def what_to_render_as(self):
+        return 'lines'
+
+
+class Evaluate(unittest.TestCase):
+    def test_square_vector(self):
+        sq = Square(VectorRenderer())
+        self.assertEqual(str(sq), 'Drawing Square as lines')
+
+    def test_pixel_triangle(self):
+        tr = Triangle(RasterRenderer())
+        self.assertEqual(str(tr), 'Drawing Triangle as pixels')
